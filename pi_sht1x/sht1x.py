1098c8a5e82e984a4f5c7275cb42c84be5c5dc7b
Liao Bin <liao_bin@189.cn>
2021-09-23 22:15:55 +0800

    Update unit * to °

--- pi_sht1x/sht1x.py
+++ pi_sht1x/sht1x.py
@@ -171,7 +171,7 @@ class SHT1x:
         self.temperature_fahrenheit = round(raw_temperature * COF.D2_SO_F[self._resolution[0]] +
                                             COF.D1_VDD_F[self.vdd], 2)
 
-        self.logger.info('Temperature: {0}*C [{1}*F]'.format(self.temperature_celsius, self.temperature_fahrenheit))
+        self.logger.info('Temperature: {0}°C [{1}°F]'.format(self.temperature_celsius, self.temperature_fahrenheit))
         return self.temperature_celsius
 
     def read_humidity(self, temperature=None):
@@ -231,7 +231,7 @@ class SHT1x:
         ew = (m * temperature) / (tn + temperature)
         self.dew_point = round(tn * (log_humidity + ew) / m - (log_humidity + ew), 2)
 
-        self.logger.info('Dew Point: {0}*C'.format(self.dew_point))
+        self.logger.info('Dew Point: {0}°C'.format(self.dew_point))
         return self.dew_point
 
     def _send_command(self, measurement=True):
@@ -557,7 +557,7 @@ class SHT1x:
         humidity = self.humidity if self.humidity is not None else '-'
         dew_point = self.dew_point if self.dew_point is not None else '-'
 
-        return 'Temperature: {0}*C [{1}*F]\nRelative Humidity: {2}%\nDew Point: {3}*C\n'.format(celsius, fahrenheit,
+        return 'Temperature: {0}°C [{1}°F]\nRelative Humidity: {2}%\nDew Point: {3}°C\n'.format(celsius, fahrenheit,
                                                                                                 humidity, dew_point)
 
 
